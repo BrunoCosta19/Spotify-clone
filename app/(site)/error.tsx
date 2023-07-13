103e97e150404bedb734f20e88f9fd9aff261456
Bruno Costa <102918628+BrunoCosta19@users.noreply.github.com>
2023-07-13 10:46:59 -0300

    Spotify Clone

---
+++ app/(site)/error.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Box from "@/components/Box";
+
+const Error = () => {
+  return ( 
+    <Box className="h-full flex items-center justify-center">
+      <div className="text-neutral-400">
+      Algo deu errado.
+      </div>
+    </Box>
+  );
+}
+ 
+export default Error;
